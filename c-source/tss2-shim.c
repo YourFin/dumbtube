2711728e6b535f97897ea6a995259f077d586ac6
YourFin <src@patrickn.co>
2025-09-05 01:32:14 -0700

    chore: clean up comment wording

--- c-source/tss2-shim.c
+++ c-source/tss2-shim.c
@@ -2,6 +2,6 @@
 #include <tss2/tss2_fapi.h>
 
 // `ForeignPtr a` wants a:
-// `FunPtr (Ptr a -> IO ())` for cleanup, so we define a small one here
-// to help.
+// `FunPtr (Ptr a -> IO ())` for cleanup, so we define a small helper
+// here to add a pointer layer
 void tss_shim_Fapi_Finalize(FAPI_CONTEXT *context) { Fapi_Finalize(&context); }
